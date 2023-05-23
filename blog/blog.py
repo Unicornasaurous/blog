4b35e30f9d3ee6151c52d12f695592e6cd9d26c3
jimothy <timothynewton1998@gmail.com>
2023-05-22 23:00:56 -0400

    added index view in blog blueprint

--- blog/blog.py
+++ blog/blog.py
@@ -1,2 +1,7 @@
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 
+bp = Blueprint("blog", __name__, url_prefix="/blog")
 
+@bp.route("/", methods=["POST", "GET"])
+def index():
+    return render_template('blog/index.html')
