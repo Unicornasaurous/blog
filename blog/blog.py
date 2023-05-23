49c509bf3efeed7965c710d97f62696b326faf4b
jimothy <timothynewton1998@gmail.com>
2023-05-23 00:50:44 -0400

    added createtopic view

--- blog/blog.py
+++ blog/blog.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-
+from .db import get_db
 bp = Blueprint("blog", __name__, url_prefix="/blog")
 
 @bp.route("/", methods=["POST", "GET"])
 def index():
     return render_template('blog/index.html')
+
+@bp.route("/createtopic", methods=["POST", "GET"])
+def createTopic():
+    return render_template('blog/createtopic.html')
