f1bbaba3674a39e6bd2adbc66d024d724c27cb80
jimothy <timothynewton1998@gmail.com>
2023-05-24 23:30:45 -0400

    added create topic and topic views

--- blog/blog.py
+++ blog/blog.py
@@ -4,8 +4,28 @@ bp = Blueprint("blog", __name__, url_prefix="/blog")
 
 @bp.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('blog/index.html')
+    db = get_db()
+
+    topics = db.execute("SELECT * FROM topic").fetchall()
+
+    return render_template('blog/index.html', topics = topics)
 
 @bp.route("/createtopic", methods=["POST", "GET"])
 def createTopic():
+    db = get_db()
+    if request.method == "POST":
+        topic = request.form['topic']
+        
+        db.execute("INSERT INTO topic (title) VALUES (?)", (topic,))
+        db.commit()
+
+        return redirect(url_for('blog.index'))
+    
     return render_template('blog/createtopic.html')
+
+@bp.route("/topic/<topic_title>")
+def topic(topic_title):
+    db = get_db()
+
+    posts = ""
+    return render_template('blog/topic.html', topic_title=topic_title)
