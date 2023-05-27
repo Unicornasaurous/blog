d8b4d85a3f7df3fefa0c91861a93108905a1b2f0
jimothy <timothynewton1998@gmail.com>
2023-05-26 21:18:20 -0400

    added views for deleting and edting topics

--- blog/blog.py
+++ blog/blog.py
@@ -14,18 +14,46 @@ def index():
 def createTopic():
     db = get_db()
     if request.method == "POST":
-        topic = request.form['topic']
-        
-        db.execute("INSERT INTO topic (title) VALUES (?)", (topic,))
+        title = request.form['title']
+        desc = request.form['desc']
+        db.execute("INSERT INTO topic (title, desc) VALUES (?, ?)", (title, desc))
         db.commit()
 
         return redirect(url_for('blog.index'))
     
     return render_template('blog/createtopic.html')
 
-@bp.route("/topic/<topic_title>")
+@bp.route("/<topic_title>")
 def topic(topic_title):
     db = get_db()
-
+    id = request.args.get('id')
+    print(f"Topic ID: {id}")
     posts = ""
     return render_template('blog/topic.html', topic_title=topic_title)
+
+@bp.route("/<int:id>/delete")
+def delete_topic(id):
+    db = get_db()
+
+    db.execute("DELETE FROM topic WHERE id = ?", (id,))
+    db.commit()
+
+    return redirect(url_for('blog.index'))
+
+@bp.route("/<int:id>/edit", methods=["POST", "GET"])
+def edit_topic(id):
+    db = get_db()
+    topic = db.execute("SELECT * FROM topic WHERE id = ?", (id,)).fetchone()
+
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+
+        db.execute("UPDATE topic SET (title, desc) = (?, ?)", (title, desc))
+        db.commit()
+
+        return redirect(url_for('blog.index'))
+
+    return render_template('blog/createtopic.html', topic=topic)
+
+    
