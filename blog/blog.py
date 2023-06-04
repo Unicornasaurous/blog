6e48086fe6175117819e6f21e3505733e500fd2f
jimothy <timothynewton1998@gmail.com>
2023-06-04 14:52:48 -0400

    fixed bugs; added views for editing/deleting posts

--- blog/blog.py
+++ blog/blog.py
@@ -32,7 +32,7 @@ def createTopic():
     
     return render_template('blog/createtopic.html')
 
-@bp.route("/<topic_title>")
+@bp.route("/<topic_title>", methods=["POST", "GET"])
 def topic(topic_title):
     """
     Topic view handles requests to view specific topics. The topic ID 
@@ -40,9 +40,13 @@ def topic(topic_title):
     """
     db = get_db()
     topic_id = request.args.get('id')
-    
     posts = db.execute("SELECT * FROM post WHERE topic_id = ?", (topic_id,)).fetchall()
-    return render_template('blog/topic.html', topic_title=topic_title, posts=posts)
+
+    if request.method == "POST":
+        if request.form['submitbutton'] == "Create Post":
+            return redirect(url_for('blog.create_post', topic_title=topic_title) + f"?topic_id={topic_id}")
+
+    return render_template('blog/topic.html', topic_title=topic_title, posts=posts, topic_id=topic_id)
 
 @bp.route("/<int:id>/delete")
 @admin_required
@@ -54,9 +58,8 @@ def delete_topic(id):
     db = get_db()
 
     db.execute("DELETE FROM topic WHERE id = ?", (id,))
+    db.execute("DELETE FROM post WHERE topic_id = ?", (id,))
     db.commit()
-
-    #TO-DO: Delete all associated posts under a topic
 
     return redirect(url_for('blog.index'))
 
@@ -72,7 +75,7 @@ def edit_topic(id):
         title = request.form['title']
         desc = request.form['desc']
 
-        db.execute("UPDATE topic SET (title, desc) = (?, ?)", (title, desc))
+        db.execute("UPDATE topic SET (title, desc) = (?, ?) WHERE id = ?", (title, desc, id))
         db.commit()
 
         return redirect(url_for('blog.index'))
@@ -80,28 +83,66 @@ def edit_topic(id):
     return render_template('blog/createtopic.html', topic=topic)
 
 @bp.route("/<topic_title>/<post_title>")
-def post():
+def post(topic_title, post_title):
     """Post view handles requests for a specific post. Post ID
     is passed as a URL arg and accessed using request.args"""
     db = get_db()
-    post_id = request.args['id']
+    post_id = request.args.get('post_id')
 
-    post = db.execute("SELECT * FROM post WHERE id = ?", (id,)).fetchone()
+    post = db.execute("SELECT * FROM post WHERE id = ?", (post_id,)).fetchone()
 
     return render_template('blog/post.html', post=post)
 
-@bp.route("/<topic_title>/createpost")
+@bp.route("/<topic_title>/createpost", methods=["POST", "GET"])
 def create_post(topic_title):
     """View handles requests to create a new post under the desired
     topic.
     """
     db = get_db()
+
     if request.method == "POST":
         title = request.form['title']
         body = request.form['body']
-        db.execute("INSERT INTO topic (title, desc) VALUES (?, ?)", (title, body))
+        topic_id = request.args.get('topic_id')
+        user_id = session.get('user_id')
+        print(title, body, topic_id)
+        db.execute("INSERT INTO post (title, body, topic_id, author_id) VALUES (?, ?, ?, ?)", 
+                   (title, body, topic_id, user_id))
         db.commit()
-
-        return redirect(url_for('blog.index'))
+        post_id = db.execute("SELECT id FROM post WHERE title = ? AND body = ?", (title, body)
+                             ).fetchone()['id']
+        return redirect(url_for('blog.post', topic_title=topic_title, post_title=title) + f"?post_id={post_id}")
     
     return render_template('blog/createpost.html')
+
+@bp.route("/<topic_title>/<int:id>/delete")
+@admin_required
+def delete_post(topic_title, id):
+    """
+    Handles requests to delete a certain post. The post ID is passed as a URL
+    variable from the user and used to delete the desired post. 
+    """
+    db = get_db()
+
+    db.execute("DELETE FROM post WHERE id = ?", (id,))
+    db.commit()
+
+    return redirect(url_for('blog.topic', topic_title=topic_title))
+
+@bp.route("/<topic_title>/<int:id>/edit", methods=["POST", "GET"])
+@admin_required
+def edit_post(topic_title, id):
+    """View handles requests to edit post. It allows users to 
+    change the values associated with a post, such as title and body"""
+    db = get_db()
+    post = db.execute("SELECT * FROM post WHERE id = ?", (id,)).fetchone()
+
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+
+        db.execute("UPDATE post SET (title, body) = (?, ?) WHERE id = ?", (title, body, post['id']))
+        db.commit()
+
+        return redirect(url_for('blog.post', topic_title=topic_title, post_title=title)+f"?post_id={post['id']}")
+    return render_template("blog/createpost.html", post=post)
